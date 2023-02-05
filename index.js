0005b46e5798e6db668056a36be32f9e661e2688
johnchen383 <john.bm.chen@gmail.com>
2023-02-05 16:03:17 +1100

    fix: add cors to socket

--- index.js
+++ index.js
@@ -3,7 +3,11 @@ const app = express();
 const http = require('http');
 const server = http.createServer(app);
 const { Server } = require("socket.io");
-const io = new Server(server);
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000"
+  }
+});
 
 // Use .env [e.g. process.env.NOTION_TOKEN]
 require("dotenv").config();
